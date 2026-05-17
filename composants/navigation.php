5c3b13ed258cbc90519358a7bbac24386549e515
aichasoda2001-ops <aichasoda2001@gmail.com>
2026-05-17 03:07:23 +0000

    Add files via upload

---
+++ composants/navigation.php
@@ -0,0 +1,19 @@
+<?php
+// Détecter automatiquement la page actuelle
+$page_courante = basename($_SERVER['PHP_SELF']);
+?>
+
+<nav>
+  <div class="logo">ASTOU<span>.</span></div>
+  <div class="nav-links">
+      <a href="https://aichasoda2001-ops.github.io/portof/">A propos</a>
+            <a href="https://aichasoda2001-ops.github.io/porto/">Projets</a>
+            <a href="https://aichasoda2001-ops.github.io/port/">Contacts</a>
+  </div>
+</nav>
+
+
+
+
+
+
